167ddf52dded383d5c725db4f675ce9bef399ca4
Anastasiia K <ana@Anastasiias-MacBook-Air.local>
2026-04-20 20:11:32 +0300

    Modified test_transformations.py

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -4,7 +4,7 @@ import pytest
 from src.transformations import clean_taxi_data
 
 def test_clean_taxi_data_filters_negative_distance(spark):
-    # Створюємо тестові дані
+    
     data = [("2025-01-01 10:00:00", "2025-01-01 10:10:00", -5.0, 10.0), 
             ("2025-01-01 11:00:00", "2025-01-01 11:10:00", 2.0, 15.0)]   
     columns = ["tpep_pickup_datetime", "tpep_dropoff_datetime", "trip_distance", "total_amount"]
